b0bba4c9e24fa93ea5753ab6780a25576ab636e2
Tafadzwa Katsidzira <tafadzwakatsidzira@Tafadzwas-MBP-2.home>
2019-12-21 01:06:30 -0500

    created display activity and serizaliezed model for intent

--- app/src/main/java/com/example/fragappfromscratchrevisted/DisplayActivity.java
+++ app/src/main/java/com/example/fragappfromscratchrevisted/DisplayActivity.java
@@ -1,13 +1,31 @@
 package com.example.fragappfromscratchrevisted;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.example.fragappfromscratchrevisted.model.Planet;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.Serializable;
+
+import static com.example.fragappfromscratchrevisted.view.PlanetViewHolder.PLANET_INSTANCE;
+
 public class DisplayActivity extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_display);
+
+        Bundle args = getIntent().getExtras();
+        assert args != null;
+        Planet planet = (Planet) args.getSerializable(PLANET_INSTANCE);
+
+
+
     }
 }
--- app/src/main/java/com/example/fragappfromscratchrevisted/view/PlanetViewHolder.java
+++ app/src/main/java/com/example/fragappfromscratchrevisted/view/PlanetViewHolder.java
@@ -7,11 +7,12 @@ import android.support.v7.widget.RecyclerView;
 import android.view.View;
 import android.widget.TextView;
 
+import com.example.fragappfromscratchrevisted.DisplayActivity;
 import com.example.fragappfromscratchrevisted.R;
 import com.example.fragappfromscratchrevisted.model.Planet;
 
 public class PlanetViewHolder extends RecyclerView.ViewHolder {
-    private static final String PLANET_INSTANCE = "planet_instance";
+    public static final String PLANET_INSTANCE = "planet_instance";
     private TextView planetNameTv;
 
     public PlanetViewHolder(@NonNull View itemView) {
@@ -25,9 +26,10 @@ public class PlanetViewHolder extends RecyclerView.ViewHolder {
         planetNameTv.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent();
-                intent.putExtra(PLANET_INSTANCE, planet);
-                v.getContext().startActivity(intent, DisplayActivity.class);
+                Intent intent = new Intent(v.getContext(), DisplayActivity.class);
+                Bundle args = new Bundle();
+                args.putSerializable(PLANET_INSTANCE, planet);
+                v.getContext().startActivity(intent.putExtras(args));
             }
         });
     }
